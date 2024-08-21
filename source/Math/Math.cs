39436cef8705469536d34dd0f8764726ab498023
Chookees <arturzubert@web.de>
2024-08-21 16:49:07 +0200

    Added Math

---
+++ source/Math/Math.cs
@@ -0,0 +1,17 @@
+﻿namespace FunctionProvider.Math
+{
+    using SM = System.Math;
+
+    public class Math
+    {
+        public static double Pi()
+        {
+            return SM.PI;
+        }
+
+        public static decimal RoundingToTwo(decimal numberToRound)
+        {
+            return SM.Round(numberToRound, 2);
+        }
+    }
+}
