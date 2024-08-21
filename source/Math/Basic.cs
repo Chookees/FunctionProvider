432c5b67d185f20c573f6b039eeb77332aa23f6c
Chookees <arturzubert@web.de>
2024-08-21 16:57:48 +0200

    Added comments and improved code

--- source/Math/Basic.cs
+++ source/Math/Basic.cs
@@ -4,11 +4,21 @@
 
     public class Basic
     {
-        public static double Pi()
-        {
-            return SM.PI;
-        }
+        /// <summary>
+        /// 3.14159265358979323846
+        /// </summary>
+        public static double Pi => SM.PI;
 
+        /// <summary>
+        /// 2.7182818284590452354
+        /// </summary>
+        public static double E => SM.E;
+
+        /// <summary>
+        /// Rounds a decimal to two.
+        /// </summary>
+        /// <param name="numberToRound">Number to be rounded.</param>
+        /// <returns>Rounded number as decimal.</returns>
         public static decimal RoundingToTwo(decimal numberToRound)
         {
             return SM.Round(numberToRound, 2);
