155d185d74adfc6c9fb48bffccab2a58f8428011
Chookees <arturzubert@web.de>
2024-08-21 16:03:18 +0200

    Added Directory.cs to IO

---
+++ FunctionProvider/IO/Directory.cs
@@ -0,0 +1,11 @@
+﻿namespace FunctionProvider.IO
+{
+    using SIO = System.IO;
+    public class Directory
+    {
+        public static DriveInfo[] GetDrives()
+        {
+            return SIO.DriveInfo.GetDrives();
+        }
+    }
+}
